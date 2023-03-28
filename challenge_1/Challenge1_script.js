f2248c754b883fde9d407d2de0b6073858a0ef23
HermanNgwenya <hermanbngwenya@gmail.com>
2023-03-28 13:48:56 +0200

    IWA_02

---
+++ challenge_1/Challenge1_script.js
@@ -0,0 +1,24 @@
+/* 
+
+/*This is the firstname of the user /*
+
+const user = 'John'
+
+/* This is the lastname of the user /* const surname = 'Smith'
+
+console.log(user; surname;)
+
+*/
+
+
+//Solution
+
+/* This is the firstname of the user */
+
+const user = 'John'
+
+/* This is the lastname of the user */
+
+const surname = 'Smith'
+
+console.log("user surname")
